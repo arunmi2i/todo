52aac48580a36633c18cb1814814070d0820b87b
Arun <arunkumarm982@gmail.com>
2019-09-25 21:30:55 +0530

    sub tasks added

--- WebContent/todo.js
+++ WebContent/todo.js
@@ -15,7 +15,6 @@ var id;
 	function open() {
 		document.getElementById("side-nav").style.width="20%";
 		document.getElementById("list").style.width="80%";
-		//document.getElementById("details").style.width="25%";
 		var i;
 		for(i=0; i<span.length; i++) {
 			span[i].style.display="inline";
@@ -40,8 +39,9 @@ function createObject() {
 
 
 var count = 0;
+var index = 0;
 var tasks = [];
-var id;
+var listId;
 	function addNewList(e) {
 		if(e.keyCode == 13) {
 			newList();
@@ -49,34 +49,106 @@ var id;
 	}
 
 	function newList() {
+		var step = {id:"", name:name, steps:[]};
 		var span = document.createElement("span");
-		var inputValue = document.getElementById("input").value;
+		step.id = index++;
+		step.name = document.getElementById("input").value;
 		var taskItem = document.createElement("div");
 		var img = document.createElement("img");
-		//let task = tasks.find(x => x.id === count);
-		//console.log(task.id+"task object");
 		var task = tasks[id];
 		console.log(task.id+"task object");
 		img.setAttribute("src", "round.png");
+		taskItem.id = step.id;
+		console.log(taskItem.id)
 		taskItem.appendChild(img);
 		taskItem.style.padding = "2% 5%";
-		console.log(inputValue);
-		span.innerHTML = inputValue;
+		console.log(step.name);
+		span.innerHTML = step.name;
 		taskItem.appendChild(span);
-		if(inputValue == '') {
+		if(step.name == '') {
 		} else {
 			document.getElementById("myList").appendChild(taskItem);
-			task.data.push(inputValue);
+			task.data.push(step);
 		}
 		document.getElementById("input").value="";
+		console.log(taskItem)
+		taskItem.addEventListener("click",myList.bind(taskItem));
+		//taskItem.addEventListener("click",demo.bind(taskItem));
 		show();
 	}
+
+	function demo() {
+		listId = this.id;
+		console.log(id+ "id"+this.id)
+		console.log(list+"====="+span+"++++")
+		console.log(list[this.id].steps)
+	}
+	function myList() {
+		var headDiv= document.getElementById("head");
+		headDiv.innerHTML = "";
+		document.getElementById("steps").innerHTML = "";
+		var img = document.createElement("img");
+		img.setAttribute("src", "round.png");
+		listId = this.id;
+		console.log(id+ "id"+this.id)
+		var span = document.createElement('span');
+		var list = tasks[id].data;
+		console.log(list+"====="+span+"++++")
+		console.log(list[this.id].steps)
+		span.innerHTML = list[this.id].name;
+		console.log(span);
+		headDiv.appendChild(img);
+		headDiv.appendChild(span);
+		showSteps();
+	}
 	
+	function showSteps() {
+		var steps = tasks[id].data[listId].steps;
+		console.log(steps)
+		for(i=0; i<steps.length; i++) {
+			console.log(steps[i])
+			var span = document.createElement("span");
+			var newDiv = document.createElement("div");
+			var img = document.createElement("img");
+			img.setAttribute("src", "round.png");
+			var value = steps[i];
+			span.innerHTML = value;
+			newDiv.append(img);
+			newDiv.append(span);
+			console.log(document.getElementById("steps"));
+			document.getElementById("steps").appendChild(newDiv);
+		}
+	}
+
 	function show() {
 		for(i=0;i<tasks.length;i++) {
 			console.log(tasks);
 		}
 	}
+document.getElementById("newStep").addEventListener("keydown", addStep);
+	function addStep(e) {
+		if(e.keyCode == 13 && document.getElementById("newStep").value != "") {
+			newStep();
+		}
+	}
+
+	function newStep() {
+		var span = document.createElement("span");
+		var newDiv = document.createElement("div");
+		var img = document.createElement("img");
+		img.setAttribute("src", "round.png");
+		var value = document.getElementById("newStep").value;
+		console.log(listId);
+		tasks[id].data[listId].steps.push(value);
+		console.log(tasks[id].data[listId].steps)
+		span.innerHTML = value;
+		newDiv.append(img);
+		newDiv.append(span);
+		console.log(document.getElementById("steps"));
+		document.getElementById("steps").appendChild(newDiv);
+		document.getElementById("newStep").value = "";
+	}
+
 document.getElementById("newTask").addEventListener("keydown", addNewTask);
 	function addNewTask(e) {
 		if(e.keyCode == 13 && document.getElementById("newTask").value != "") {
@@ -109,14 +181,12 @@ document.getElementById("newTask").addEventListener("keydown", addNewTask);
 
 	function myTask() {
 		var listHead = document.getElementById("list-head");
-		//var headValue = listHead.querySelector("h2");
 		id = this.id;
 		console.log(id)
 		var name = tasks[id].name;
 		console.log(name+"sdjfhjshfdsf");
 		listHead.innerHTML="";
 		var head = document.createElement('h2');
-		//head.innerHTML = document.getElementById("newTask").value;
 		head.innerHTML = name;
 		listHead.append(head);
 		console.log(count+"Count");
@@ -128,25 +198,28 @@ document.getElementById("newTask").addEventListener("keydown", addNewTask);
 		console.log(id+"datd"+tasks[id].data);
 		var list = tasks[id].data;
 		console.log(list);
+		index = 0;
+		console.log(index)
 		for(i=0; i<list.length; i++) {
 			var taskItem = document.createElement("div");
+			taskItem.id = index++;
 			var img = document.createElement("img");
 			img.setAttribute("src", "round.png");
 			taskItem.appendChild(img);
 			taskItem.style.padding = "2% 5%";
-			console.log(list[i]+"LEN"+list.length)
+			console.log(list[i].name+"LEN"+list.length)
 			var span = document.createElement("span");
-			span.innerHTML=list[i];
+			span.innerHTML=list[i].name;
+			var cb = document.createElement('input');
+		    cb.type = 'checkbox';
+		    taskItem.appendChild(cb);
+			console.log(span)
 			taskItem.appendChild(span);
 			document.getElementById("myList").appendChild(taskItem);
-		}
-		list.forEach(getTaskValue);
-		function getTaskValue(task, index) {
-			span.innerHTML = task;
-			console.log(task)
+			taskItem.addEventListener("click",myList.bind(taskItem));
 		}
 	}
-	
+
 	function remove() {
 		var myList = document.getElementById("myList");
 		while(myList.hasChildNodes()) {
